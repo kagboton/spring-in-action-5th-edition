cde6a45df1559f4f615787b037eb38f25adee02c
Kilian Agboton <kilian.agboton@gmail.com>
2021-04-29 18:20:17 -0400

    tacos-cloud :: udpated domain classes

---
+++ tacoscloud/src/main/java/io/kagboton/tacoscloud/domain/Ingredient.java
@@ -0,0 +1,34 @@
+package io.kagboton.tacoscloud.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public Ingredient(String id, String name, Type type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public static enum Type{
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}

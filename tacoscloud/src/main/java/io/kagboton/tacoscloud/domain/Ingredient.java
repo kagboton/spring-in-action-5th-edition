3e25eb2503c294cc9532ee6287e67624ba31d11a
Kilian Agboton <kilian.agboton@gmail.com>
2021-05-04 17:56:08 -0400

    tacos-cloud :: added ingedient type to string converter to Ingredient clas

--- tacoscloud/src/main/java/io/kagboton/tacoscloud/domain/Ingredient.java
+++ tacoscloud/src/main/java/io/kagboton/tacoscloud/domain/Ingredient.java
@@ -1,11 +1,14 @@
 package io.kagboton.tacoscloud.domain;
 
+import io.kagboton.tacoscloud.controller.IngredientTypeConverter;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @RequiredArgsConstructor
@@ -13,8 +16,10 @@ import javax.persistence.Entity;
 @Entity
 public class Ingredient {
 
+    @Id
     private final String id;
     private final String name;
+    @Convert(converter = IngredientTypeConverter.class)
     private final Type type;
 
     public static enum Type{

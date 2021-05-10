0696749a2f3aa3db14d60afbf16e2698f0974424
Kilian Agboton <kilian.agboton@gmail.com>
2021-05-10 13:15:02 -0400

    tacos-cloud :: jdbc authentication user store

--- tacoscloud/src/main/java/io/kagboton/tacoscloud/config/SecurityConfig.java
+++ tacoscloud/src/main/java/io/kagboton/tacoscloud/config/SecurityConfig.java
@@ -1,21 +1,31 @@
 package io.kagboton.tacoscloud.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.
-                inMemoryAuthentication()
-                .withUser("toto")
-                .password("{noop}toto")
-                .authorities("ROLE_USER");
+                jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery( // overrride the authentication and basic authorization queries
+                        "select username, password, enabled from Users where username=?")
+                .authoritiesByUsernameQuery(
+                        "select username, authority from UserAuthorities where username=?")
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
 

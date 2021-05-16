576e7720794fef058a823cea8b002b3833ca1dc9
Kilian Agboton <kilian.agboton@gmail.com>
2021-05-16 01:58:55 -0400

    tacos-cloud :: updated web and security configuration classes

--- tacoscloud/src/main/java/io/kagboton/tacoscloud/config/SecurityConfig.java
+++ tacoscloud/src/main/java/io/kagboton/tacoscloud/config/SecurityConfig.java
@@ -1,32 +1,62 @@
 package io.kagboton.tacoscloud.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Qualifier("userRepositoryUserDetailsService")
     @Autowired
-    DataSource dataSource;
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.
-                jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery( // overrride the authentication and basic authorization queries
-                        "select username, password, enabled from Users where username=?")
-                .authoritiesByUsernameQuery(
-                        "select username, authority from UserAuthorities where username=?")
-                .passwordEncoder(new BCryptPasswordEncoder());
+        auth
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                    .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**", "/login*").access("permitAll")
+
+            .and() // custom login page
+                .formLogin()
+                    .loginPage("/login")
+
+                .and()
+                .logout()
+                    .logoutSuccessUrl("/")
+
+            .and() // make H2-Console non-secured; for debug purposes
+                .csrf()
+                    .ignoringAntMatchers("/h2-console/**")
+
+            .and() // allow pages to be loaded in frames from the same origin; needed for H2-Console
+                .headers()
+                    .frameOptions()
+                        .sameOrigin(); // csrfIgnore
     }
 }
-
-

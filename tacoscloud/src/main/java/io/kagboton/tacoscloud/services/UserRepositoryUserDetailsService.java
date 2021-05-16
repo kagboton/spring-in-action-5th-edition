573ec78b6e95014bb499686f4803b3aad3734e59
Kilian Agboton <kilian.agboton@gmail.com>
2021-05-16 02:00:57 -0400

    tacos-cloud :: updated services

---
+++ tacoscloud/src/main/java/io/kagboton/tacoscloud/services/UserRepositoryUserDetailsService.java
@@ -0,0 +1,32 @@
+package io.kagboton.tacoscloud.services;
+
+import io.kagboton.tacoscloud.domain.User;
+import io.kagboton.tacoscloud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserRepositoryUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+
+        if(user != null) return user;
+
+        throw new UsernameNotFoundException("User '"+ username + "' not found");
+
+    }
+
+}

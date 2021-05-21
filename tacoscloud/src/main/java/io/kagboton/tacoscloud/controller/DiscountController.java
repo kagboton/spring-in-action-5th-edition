c6cf2a6df7522c1e5a717dcc0e7b8eeca67c7b06
Kilian Agboton <kilian.agboton@gmail.com>
2021-05-21 00:52:25 -0400

    tacos-cloud :: added discounts controller

---
+++ tacoscloud/src/main/java/io/kagboton/tacoscloud/controller/DiscountController.java
@@ -0,0 +1,31 @@
+package io.kagboton.tacoscloud.controller;
+
+import java.util.Map;
+
+import io.kagboton.tacoscloud.utils.DiscountCodeProps;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/discounts")
+public class DiscountController {
+
+  private DiscountCodeProps discountProps;
+
+  public DiscountController(DiscountCodeProps discountProps) {
+    this.discountProps = discountProps;
+  }
+  
+  @GetMapping
+  public String displayDiscountCodes(Model model) {
+    
+    Map<String, Integer> codes = discountProps.getCodes();
+    model.addAttribute("codes", codes);
+    
+    return "discountList";
+  }
+  
+}

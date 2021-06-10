78959d0eb80e036bd09841557e2d189699f2cf65
Kilian Agboton <kilian.agboton@gmail.com>
2021-06-09 23:13:24 -0400

    tacos-cloud :: messaging view controllers config

---
+++ tacoscloud/src/main/java/io/kagboton/tacoscloud/config/WebConfigMessaging.java
@@ -0,0 +1,17 @@
+package io.kagboton.tacoscloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile({"jms-template", "rabbitmq-template"})
+@Configuration
+public class WebConfigMessaging implements WebMvcConfigurer {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addRedirectViewController("/", "/orders/receive");
+  }
+  
+}
